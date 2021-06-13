83f494571edc86c9dc6b8951508e06e331019061
krapjost <krapjost@gmail.com>
2021-06-13 21:50:38 +0900

    font https로 설정 변경

--- src/Tailwindcss.svelte
+++ src/Tailwindcss.svelte
@@ -8,8 +8,8 @@
     font-weight: 700;
     src:
       url('assets/fonts/GmarketSansTTF/GmarketSansTTFBold.ttf') format('truetype'),
-      url('http://script.ebay.co.kr/fonts/GmarketSansBold.woff2') format('woff2'),
-      url('http://script.ebay.co.kr/fonts/GmarketSansBold.woff') format('woff');
+      url('https://script.ebay.co.kr/fonts/GmarketSansBold.woff2') format('woff2'),
+      url('https://script.ebay.co.kr/fonts/GmarketSansBold.woff') format('woff');
   }
   @font-face {
     font-family: 'Gmarket Sans';
@@ -17,8 +17,8 @@
     font-weight: 500;
     src:
       url('assets/fonts/GmarketSansTTF/GmarketSansTTFMedium.ttf') format('truetype'),
-      url('http://script.ebay.co.kr/fonts/GmarketSansMedium.woff2') format('woff2'),
-      url('http://script.ebay.co.kr/fonts/GmarketSansMedium.woff') format('woff');
+      url('https://script.ebay.co.kr/fonts/GmarketSansMedium.woff2') format('woff2'),
+      url('https://script.ebay.co.kr/fonts/GmarketSansMedium.woff') format('woff');
   }
   @font-face {
     font-family: 'Gmarket Sans';
@@ -26,8 +26,8 @@
     font-weight: 300;
     src: 
       url('assets/fonts/GmarketSansTTF/GmarketSansTTFLight.ttf') format('truetype'),
-      url('http://script.ebay.co.kr/fonts/GmarketSansLight.woff2') format('woff2'),
-      url('http://script.ebay.co.kr/fonts/GmarketSansLight.woff') format('woff');
+      url('https://script.ebay.co.kr/fonts/GmarketSansLight.woff2') format('woff2'),
+      url('https://script.ebay.co.kr/fonts/GmarketSansLight.woff') format('woff');
   }
 
 </style>
