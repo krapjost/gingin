6e70ce1661ff8e192d514a9c40b7911c6c29f4ce
krapjost <krapjost@gmail.com>
2021-06-13 21:48:47 +0900

    font 설정 변경

--- src/Tailwindcss.svelte
+++ src/Tailwindcss.svelte
@@ -6,25 +6,28 @@
     font-family: 'Gmarket Sans';
     font-style: normal;
     font-weight: 700;
-    src: local('Gmarket Sans Bold'), local('GmarketSans-Bold')
-     ,url('http://script.ebay.co.kr/fonts/GmarketSansBold.woff2') format('woff2')
-     ,url('http://script.ebay.co.kr/fonts/GmarketSansBold.woff') format('woff')
+    src:
+      url('assets/fonts/GmarketSansTTF/GmarketSansTTFBold.ttf') format('truetype'),
+      url('http://script.ebay.co.kr/fonts/GmarketSansBold.woff2') format('woff2'),
+      url('http://script.ebay.co.kr/fonts/GmarketSansBold.woff') format('woff');
   }
   @font-face {
     font-family: 'Gmarket Sans';
     font-style: normal;
     font-weight: 500;
-    src: local('Gmarket Sans Medium'), local('GmarketSans-Medium')
-     ,url('http://script.ebay.co.kr/fonts/GmarketSansMedium.woff2') format('woff2')
-     ,url('http://script.ebay.co.kr/fonts/GmarketSansMedium.woff') format('woff')
+    src:
+      url('assets/fonts/GmarketSansTTF/GmarketSansTTFMedium.ttf') format('truetype'),
+      url('http://script.ebay.co.kr/fonts/GmarketSansMedium.woff2') format('woff2'),
+      url('http://script.ebay.co.kr/fonts/GmarketSansMedium.woff') format('woff');
   }
   @font-face {
     font-family: 'Gmarket Sans';
     font-style: normal;
     font-weight: 300;
-    src: local('Gmarket Sans Light'), local('GmarketSans-Light')
-     ,url('http://script.ebay.co.kr/fonts/GmarketSansLight.woff2') format('woff2')
-     ,url('http://script.ebay.co.kr/fonts/GmarketSansLight.woff') format('woff')
+    src: 
+      url('assets/fonts/GmarketSansTTF/GmarketSansTTFLight.ttf') format('truetype'),
+      url('http://script.ebay.co.kr/fonts/GmarketSansLight.woff2') format('woff2'),
+      url('http://script.ebay.co.kr/fonts/GmarketSansLight.woff') format('woff');
   }
 
 </style>
