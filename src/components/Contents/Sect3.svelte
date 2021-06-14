0083cebc6cb077622b45a1e3e4b6c5d83c064386
krapjost <krapjost@gmail.com>
2021-06-14 22:27:52 +0900

    css 수정

--- src/components/Contents/Sect3.svelte
+++ src/components/Contents/Sect3.svelte
@@ -26,7 +26,7 @@
 <div class="border-t-2 border-gin mt-12 md:mt-24 w-full"/>
 {#if direction === "right"}
 <section class="text-gin">
-    <div class="container mx-auto my-auto flex md:py-8 md:flex-row flex-col items-center md:pt-24 pt-12">
+    <div class="container mx-auto my-auto flex md:flex-row flex-col items-center md:pt-24 pt-12">
       
       <div class="lg:flex-grow md:w-1/2 lg:pr-14 md:pr-12 flex flex-col md:items-end md:text-right md:mb-0 items-center text-center lg:items-end lg:text-right">
         <h1 class="sm:text-3xl text-2xl mb-4 h-auto font-bold">
@@ -35,7 +35,7 @@
         <p class="my-4 md:text-xl text-lg font-thin leading-relaxed whitespace-pre-line">{subText}</p>
       </div>
       
-      <div class="my-4 md:w-1/2 w-5/6 flex items-center">
+      <div class="my-4 lg:pl-5 md:w-1/2 w-5/6 flex items-center">
         <div class="w-full aspect-w-16 aspect-h-9">
           <img class="object-cover object-center rounded" alt="상품 사진" src={imgs.s}>
         </div>
@@ -45,9 +45,9 @@
   </section>
 {:else if direction === "left"}
   <section class="text-gin ">
-    <div class="container mx-auto my-auto flex md:py-8 md:flex-row flex-col items-center md:pt-24 pt-12">
+    <div class="container mx-auto my-auto flex md:flex-row flex-col items-center md:pt-24 pt-12">
       
-      <div class="my-4 md:w-1/2 w-5/6 mb-10 md:mb-0 flex items-center">
+      <div class="my-4 lg:pr-5 md:w-1/2 w-5/6 flex items-center">
         <div class="w-full aspect-w-16 aspect-h-9">
           <img class="object-cover object-center rounded" alt="상품 사진 2" src={imgs.s}>
         </div>
